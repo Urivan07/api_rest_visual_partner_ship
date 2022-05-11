c1a3d04ef2e2e36b1fbe1c4df955ced7de7dc16c
Urivan <urivan07@gmail.com>
2022-05-10 22:19:28 -0500

    Se agrega metodo getStudentsByCredits para obtener los estudiantes con creditos mayores a 500, se agrega prueba unitaria

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -1,5 +1,6 @@
 const StudentService = require("./../../lib/services/StudentService");
 const Reader = require("./../../lib/utils/reader");
+const students = Reader.readJsonFile("visualpartners.json");
 
 describe("Test StudentService", () => {
     test("Requerimiento 1 consultar todos los estudiantes con todos sus campos", ()=>{
@@ -11,9 +12,14 @@ describe("Test StudentService", () => {
         expect(students.length).toBeGreaterThan(0);
     });
     test("Requerimiento 2 consultar los emails de todos los estudiantes que tengan certificación", ()=>{
-        const students = Reader.readJsonFile("visualpartners.json");
         const mailsOfStudents = StudentService.getStudentsByCerification(students, true);
         expect(mailsOfStudents.length).toBeGreaterThan(0);
         expect(mailsOfStudents).toContain("Todd@visualpartnership.xyz");
+    });
+    test("Requerimiento 3 consultar todos los estudiantes que tengan credits mayor a 500", ()=>{
+        const StudentsByCredits = StudentService.getStudentsByCredits(students, 500);
+        expect(StudentsByCredits[0].name).toContain("Warren");
+        expect(StudentsByCredits[0].credits).toBe(508);
+        expect(StudentsByCredits[0].enrollments).toContain("Visual Thinking Intermedio");
     });
 });
