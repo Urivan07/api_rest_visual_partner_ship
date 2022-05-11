02278df135100218d06895b3c0e8f71ea62b40a7
Urivan <urivan07@gmail.com>
2022-05-11 00:01:28 -0500

    Se agrega metodo getStudentsByCerification a clase StudentController el cual consulta al script StudentService para obtener los emails con certificado true o false, se realizan pruebas sobre el mismo metodo

--- test/controller/StudentController.test.js
+++ test/controller/StudentController.test.js
@@ -1,12 +1,22 @@
-const StudentController = require("./../../lib/controller/StudentController")
+const StudentController = require("./../../lib/controller/StudentController");
 
 describe("Test StudentController", ()=>{
-    test("Requerimiento 1 consultar todos los estudiantes con todos sus campo", () =>{
+    test("Consultar todos los estudiantes con todos sus campo", () =>{
         const students = StudentController.getStudents();
         expect(students[0]).toHaveProperty("haveCertification");
         expect(students[0]).toHaveProperty("name");
         expect(students[0]).toHaveProperty("email");
         expect(students[0]).toHaveProperty("credits");
         expect(students.length).toBeGreaterThan(0);
-    })
-})
+    });
+    test("Consultar los emails de todos los estudiantes que tengan no certificación", ()=>{
+        const mailsOfStudents = StudentController.getStudentsByCerification(false);
+        expect(mailsOfStudents.length).toBeGreaterThan(0);
+        expect(mailsOfStudents).toContain("Camacho@visualpartnership.xyz");
+    });
+    test("Consultar los emails de todos los estudiantes que tengan certificación", ()=>{
+        const mailsOfStudents = StudentController.getStudentsByCerification(true);
+        expect(mailsOfStudents.length).toBeGreaterThan(0);
+        expect(mailsOfStudents).toContain("Mcpherson@visualpartnership.xyz");
+    });
+});
