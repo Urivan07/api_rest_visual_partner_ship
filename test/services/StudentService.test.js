9911ca629f1b3e8a8fabfeb6836a7c3fb3ceaf41
Urivan <urivan07@gmail.com>
2022-05-10 21:48:30 -0500

    Se agrega prueba con error para test de github actions

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -3,6 +3,10 @@ const StudentService = require("./../../lib/services/StudentService")
 describe("Test StudentService", () => {
     test("Requerimiento 1 consultar todos los estudiantes con todos sus campos", ()=>{
         const students = StudentService.getStudents();
-        expect(students.length).toBe(51)
+        expect(students[0]).toHaveProperty("haveCertification")
+        expect(students[0]).toHaveProperty("name")
+        expect(students[0]).toHaveProperty("email")
+        expect(students[0]).toHaveProperty("username")
+        expect(students.length).toBeGreaterThan(0)
     })
 })
